301858d43e37aaa1e2e740157de16dfd5ec8d2b2
kilisoria <cristiansoria@gmail.com>
2015-01-25 23:18:49 -0300

    Updated the models and minor adjustments on the UI,

--- TVS/js/models/seriesModel.js
+++ TVS/js/models/seriesModel.js
@@ -12,12 +12,14 @@ TVS.VerMas = DS.Model.extend({
 TVS.Serie.FIXTURES = [
 {id: 1, title: "The Walking Dead", channel: "AMC"},
 {id: 2, title: "The Game of Thrones", channel: "HBO"},
-{id: 3, title: "House of Cards", channel: "HBO"},
+{id: 3, title: "House of Cards", channel: "Netflix"},
 {id:4, title: "True Detectives", channel: "HBO"},
-{id:5, title: "The Big Bang Theory", channel: "Warner"},
-{id:6, title: "Mom", channel: "Warner"}
+{id:5, title: "The Big Bang Theory", channel: "Warner Channel"},
+{id:6, title: "Mom", channel: "Warner Channel"}
 ];
 
 TVS.VerMas.FIXTURES = [
-{id:7, title: "Mom", channel: "Warner"}
+{id:7, title: "Fargo", channel: "Netflix"},
+{id:8, title: "Marvel - Agent of Shield", channel: "ABC"},
+{id:9, title: "Transparent", channel: "Amazon"},
 ];
