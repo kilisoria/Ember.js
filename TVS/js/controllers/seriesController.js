02b9448af997fb1d5c8254037a53532c02389c40
kilisoria <cristiansoria@gmail.com>
2015-02-03 12:21:03 -0300

    Updated create serie section.

--- TVS/js/controllers/seriesController.js
+++ TVS/js/controllers/seriesController.js
@@ -8,11 +8,22 @@ TVS.SeriesController = Ember.ObjectController.extend({
 });
 
 TVS.NewController = Ember.ObjectController.extend({
-	newSerie: "",
+	newSerieTitle: "",
+	newSerieChannel: "",
 	actions:{
-		add: function(){
-			var serie = this.get('newSerie');
-			console.log(serie);
+		add: function(){		
+			var serieTitle = this.get('newSerieTitle');
+			var serieChannel = this.get('newSerieChannel');
+
+			//if(Ember.computed.notEmpty(serieTitle) && Ember.computed.notEmpty(serieChannel))
+				this.store.createRecord('serie', {title: serieTitle, channel: serieChannel});
+
+			this.transitionToRoute('series');	
+			this.send('clear');	
+		},
+		clear: function(){
+			this.set('newSerieTitle', '');
+			this.set('newSerieChannel', '');
 		}
 	}
 })
