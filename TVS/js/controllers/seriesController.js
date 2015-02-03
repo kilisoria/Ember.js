0898a869c3bb4a919727bd336b1c321b3d6b3ecc
kilisoria <cristiansoria@gmail.com>
2015-02-03 16:45:08 -0300

    Added more functionality

--- TVS/js/controllers/seriesController.js
+++ TVS/js/controllers/seriesController.js
@@ -1,8 +1,20 @@
 
 TVS.SeriesController = Ember.ObjectController.extend({
+	serieWeb: '',
+
 	actions:{
 		new: function(){			
 			Ember.Route.transitionTo('new');
+		},
+		openSerieOficialWeb: function(web){
+			window.open(web,'_blank');	
+		},
+		remove: function(id){
+			this.store.find('serie', id).then(function(serie){
+				serie.deleteRecord();
+				serie.get('isDeleted');
+				serie.save();	
+			})
 		}
 	}
 });
@@ -10,13 +22,13 @@ TVS.SeriesController = Ember.ObjectController.extend({
 TVS.NewController = Ember.ObjectController.extend({
 	newSerieTitle: "",
 	newSerieChannel: "",
+
 	actions:{
 		add: function(){		
 			var serieTitle = this.get('newSerieTitle');
 			var serieChannel = this.get('newSerieChannel');
 
-			//if(Ember.computed.notEmpty(serieTitle) && Ember.computed.notEmpty(serieChannel))
-				this.store.createRecord('serie', {title: serieTitle, channel: serieChannel});
+			this.store.createRecord('serie', {title: serieTitle, channel: serieChannel});
 
 			this.transitionToRoute('series');	
 			this.send('clear');	
@@ -24,6 +36,23 @@ TVS.NewController = Ember.ObjectController.extend({
 		clear: function(){
 			this.set('newSerieTitle', '');
 			this.set('newSerieChannel', '');
+		},
+		existDataEnteredToCreateSerie: function() {
+			console.log(this.get('content.newSerieTitle'));
+			console.log(this.get('content.serieChannel'));
+			return false; 	
+		}.property('newSerieTitle', 'newSerieChannel')
+	}
+});
+
+TVS.HistorySeriesController = Ember.ObjectController.extend({
+	actions:{
+		remove: function(id){
+			this.store.find('historySerie', id).then(function(serie){
+				serie.deleteRecord();
+				serie.get('isDeleted');
+				serie.save();	
+			})
 		}
 	}
-})
+});
--- TVS/js/models/seriesModel.js
+++ TVS/js/models/seriesModel.js
@@ -1,26 +1,30 @@
 
 TVS.Serie = DS.Model.extend({
 	title: DS.attr('string'),
-	channel: DS.attr('string')
+	channel: DS.attr('string'),
+	web: DS.attr('string'),
+	status: DS.attr('string')
 });
 
 TVS.HistorySerie = DS.Model.extend({
 	title: DS.attr('string'),
-	channel: DS.attr('string')
+	channel: DS.attr('string'),
+	web: DS.attr('string'),
+	status: DS.attr('string')
 });
 
 TVS.Serie.FIXTURES = [
-{id: 1, title: "The Walking Dead", channel: "AMC"},
-{id: 2, title: "The Game of Thrones", channel: "HBO"},
-{id: 3, title: "House of Cards", channel: "Netflix"},
-{id:4, title: "True Detectives", channel: "HBO"},
-{id:5, title: "The Big Bang Theory", channel: "Warner Channel"},
-{id:6, title: "Mom", channel: "Warner Channel"}
+{id: 1, title: 'The Walking Dead', channel: 'AMC', web: 'http://www.amctv.com/shows/the-walking-dead', status: 'ongoing'},
+{id: 2, title: 'The Game of Thrones', channel: 'HBO', web: 'http://www.hbo.com/game-of-thrones', status: 'ongoing'},
+{id: 3, title: 'House of Cards', channel: 'Netflix',  web: 'http://nflx.it/16gJcMf', status: 'ongoing'},
+{id:4, title: 'True Detectives', channel: 'HBO', web: '', status: 'completed' },
+{id:5, title: 'The Big Bang Theory', channel: 'CBS', web: 'http://www.cbs.com/shows/big_bang_theory/', status: 'ongoing' },
+{id:6, title: 'Marvel - Agent of Shield', channel: 'ABC', web: 'http://abc.go.com/shows/marvels-agents-of-shield', status: 'ongoing'},
 ];
 
 TVS.HistorySerie.FIXTURES = [
-{id:7, title: "Fargo", channel: "Netflix"},
-{id:8, title: "Marvel - Agent of Shield", channel: "ABC"},
-{id:9, title: "Transparent", channel: "Amazon"},
-{id:10, title: "Flash", channel: "Warner Channel"}
+{id:1, title: 'Fargo', status: 'completed'},
+{id:2, title: 'Believe', status: 'canceled'},
+{id:3, title: 'Transparent',  status: 'completed'},
+{id:4, title: 'Flash',  status: 'ongoing'}
 ];
