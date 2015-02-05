39a2beb0f2f6d3e8c86c75872dda46a931aef6a7
kilisoria <cristiansoria@gmail.com>
2015-02-04 23:46:26 -0300

    Ember Cli

---
+++ MarvelWorld-Cli/app/router.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.route("comics");
+  this.route("movies");
+});
+
+export default Router;
