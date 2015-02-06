5a2a9c7e463bbf33b0d90f77737b76b79bca54f2
kilisoria <cristiansoria@gmail.com>
2015-02-06 09:56:01 -0300

    improvements on movies, also add coming soon hbs.

--- MarvelWorld-Cli/app/models/movie.js
+++ MarvelWorld-Cli/app/models/movie.js
@@ -2,5 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 	title: DS.attr('string'),
-	brief: DS.attr('string')
+	brief: DS.attr('string'),
+	code: function(){
+		return '000001234A#' + this.get('id');
+	}.property('id','title')
 });
---
+++ MarvelWorld-Cli/tests/unit/helpers/upper-case-test.js
@@ -0,0 +1,11 @@
+import {
+  upperCase
+} from 'marvel-world-cli/helpers/upper-case';
+
+module('UpperCaseHelper');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = upperCase(42);
+  ok(result);
+});
