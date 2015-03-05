59a4a8315c6532440bdf0c37029cc4a845550863
kilisoria <cristiansoria@gmail.com>
2015-03-05 11:04:14 -0300

    Added new project with ember latest version.
    ember: 1.10
    ember-cli: 0.1.15

--- MarvelWorld-Cli/app/controllers/tv.js
+++ MarvelWorld-Cli/app/controllers/tv.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+	tableColumns: [{name: 'id', label: 'id', order:1 },
+				   {name:'name', label:'name', order: 2}],	
+	tableData:[{id: 22, name:"test 001"}],		   
+	tableConfig:{sortProperties: ['id', 'name']}
 });
